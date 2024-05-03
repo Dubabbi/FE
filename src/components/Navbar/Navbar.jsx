6da6f4e911817f426e69496401fb7018c4c21c34
Dubabbi <thdms3160@naver.com>
2024-05-04 04:03:03 +0900

    Feat: 하단 Navbar 추가

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+//Navbar.jsx
+
+import React from "react"
+import './Navbar.css';
+
+
+export default function Footer() {
+    return(
+        <div className='footer'>
+            <div className='si__footer'>
+            <div className='si__footer section__padding'>
+                <div className='si__footer-below'>
+                    <div className='si__footer-copyright'>
+                    <p>이동 이동</p>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    )
+}
