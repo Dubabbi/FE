1560f6628cdae0f6212d39ad7acf783a90627810
Dubabbi <thdms3160@naver.com>
2024-05-04 04:01:06 +0900

    Design: 스타일, 폰트 추가

---
+++ src/style/globalStyle.js
@@ -0,0 +1,98 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --vh: 100%;
+    background-color: white;
+  }
+
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, menu, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed,
+  figure, figcaption, footer, header, hgroup,
+  main, menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    vertical-align: baseline;
+    font: inherit;
+    font-family: 'LINESeedKR-Bd';
+  }
+
+  /* Style resets for common elements */
+
+
+  * {
+    box-sizing: border-box;
+  }
+
+  html {
+    -webkit-touch-callout: none; 
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
+    scroll-behavior: smooth;
+    font-family: 'LINESeedKR-Bd', sans-serif;
+    font-size: 62.5%; /* 1rem = 10px */
+    user-select: none; /* disable text select */
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  input, button {
+    outline: none;
+    border: none;
+    background-color: transparent;
+  }
+
+  button {
+    cursor: pointer;
+    padding: 0;
+  }
+
+
+
+  
+  input {
+    appearance: none;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+
+  @font-face {
+    font-family: 'LINESeedKR-Bd';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.0/LINESeedKR-Bd.woff2') format('woff2');
+    font-weight: 100;
+    font-style: normal;
+  }
+
+  
+@font-face {
+  font-family: 'LINESeedKR-Bd';
+  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.0/LINESeedKR-Bd.woff2') format('woff2');
+  font-weight: 400;
+  font-style: normal;
+}
+  
+@font-face {
+  font-family: 'LINESeedKR-Bd';
+  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.0/LINESeedKR-Bd.woff2') format('woff2');
+  font-weight: 700;
+  font-style: normal;
+}
+
+`;
+
