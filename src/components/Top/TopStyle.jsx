130441489a13394f6d281401aadbdd5bee6836f5
Dubabbi <thdms3160@naver.com>
2024-05-04 04:02:45 +0900

    Feat: 상단바 추가

---
+++ src/components/Top/TopStyle.jsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const navHeight = '7vw';
+
+export const TopWrapper = styled.div`
+    position: fixed;
+    background-color: #45464b;
+    width : 100%;
+    height : 55px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    z-index: 1000;  
+`
+
+export const LinkWrapper = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+`
+
+export const Header = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+export const Title = styled.div`
+    font-size: 3rem;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    color: white;
+`
+
+//
+
+
+export const Avatar = styled.div`
+    margin: 0 20px 0 10px;
+`
