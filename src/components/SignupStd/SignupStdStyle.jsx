74455d991b26c9731c5cd0487f0eef928624e6a0
Dubabbi <thdms3160@naver.com>
2024-05-07 03:04:11 +0900

    Add: 회원가입 유효성 검사 추가

--- src/components/SignupStd/SignupStdStyle.jsx
+++ src/components/SignupStd/SignupStdStyle.jsx
@@ -67,33 +67,29 @@ export const LostPwMessage = styled.div`
 
 
 export const InputWrap = styled.div`
+  position: relative;
   display: flex;
   border-radius: 8px;
-  padding: 1.5rem;
-  margin-bottom: 3%;
+  padding: 1.3rem;
+  margin-bottom: 4%;
   margin-left: 10%;
   max-width: 80%;
   background-color: #EFF0F2;
   border: 2px solid #e2e0e0;
   &:focus-within {
-    border: 2px solid #4B518F;
+    border: 2px solid ${({ invalid }) => invalid ? 'red' : '#4B518F'};
   }
-  @media (max-width: 500px) { // 작은 모바일
-    padding: 1.2rem;
-  }
-
 `;
 
 export const Input = styled.input`
   width: 100%;
   outline: none;
   border: none;
-  height: 15%;
+  height: 14%;
   font-size: 1.5rem;
   font-weight: 400;
-
   &::placeholder {
-    color: #666666;
+    color: '#666666' // 플레이스홀더 색상 조정
   }
 `;
 
@@ -140,22 +136,30 @@ export const BottomButton = styled.button`
 `;
 
 export const ErrorMessageWrap = styled.div`
-  position: fixed;
-  padding-left: 5%;
-  margin-top: 1%;
-  width: 90%;
+  position: absolute;
+  bottom: -17px; // InputWrap 아래에 오류 메시지를 위치시키기 위해
+  left: 1%;
+  width: 80%;
   color: red;
-  font-size: 1.3rem;
+  font-size: 1.1rem;
+  z-index: 10; // 필요에 따라 z-index를 조정하여 다른 요소들 위에 표시되도록 함
 `;
 
 export const ImageWrap = styled.div`
     display: flex;
     justify-content: flex-end; /* 우측 정렬 */
+    justify-content: space-between;
     img {
-        margin: 10% 10 10; /* 상단 마진 유지, 좌우 마진을 0으로 설정 */
+        margin-right: 7%;
         max-width: 30%; /* 이미지의 최대 너비 제한 */
     }
+    a{
+      width: 20%;
+      margin-left: 10%;
+      margin-top: 10%;
+    }
 `
+
 
 /*
 export const NoAccount = styled.div`
