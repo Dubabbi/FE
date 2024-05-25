cd0aab633bc20c92820e85ed89966b50d243a413
Dubabbi <thdms3160@naver.com>
2024-05-25 17:12:40 +0900

    Feat: 로그인/회원가입에서 내비게이션바, 푸터 제외

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,25 +1,36 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Top from './components/Top/Top';
-import Navbar from './components/Navbar/Navbar';
-import SelectPage  from './pages/Select';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
+import Navbar from './components/Nav/Navbar';
+import Footer from './components/Footer/Footer';
+import SelectPage from './pages/Select';
 import LoginPage from './pages/Login';
 import SignupStdPage from './pages/SignupStd';
 import SignupTchrPage from './pages/SignupTchr';
 
 export default function Router() {
   return (
+    <BrowserRouter>
+      <AppWrapper />
+    </BrowserRouter>
+  );
+}
+
+function AppWrapper() {
+  const location = useLocation();
+  const hideNavBarRoutes = ['/Select', '/SignupStd', '/SignupTchr', '/']; // 숨기고 싶은 경로
+  const showNavBar = !hideNavBarRoutes.includes(location.pathname);
+  const showComponents = !hideNavBarRoutes.includes(location.pathname);
+
+  return (
     <>
-      <BrowserRouter>
-        <Top/>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/Select" element={<SelectPage />} />
-          <Route path="/SignupStd" element={<SignupStdPage />} />
-          <Route path="/SignupTchr" element={<SignupTchrPage />} />              
-        </Routes>
-        <Navbar/>
-      </BrowserRouter>
+      {showNavBar && <Navbar />}
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/select" element={<SelectPage />} />
+        <Route path="/signupstd" element={<SignupStdPage />} />
+        <Route path="/signuptchr" element={<SignupTchrPage />} />
+      </Routes>
+      {showComponents && <Footer />}
     </>
   );
 }
