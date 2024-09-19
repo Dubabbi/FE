4e8071dbad44329e37a2497d62752a6e63608cba
Pjh01 <estelle0102@naver.com>
2024-09-19 16:02:15 +0900

    Feat: 피드백 상세 페이지 수정

--- src/components/StudyLesson/Template1Std.jsx
+++ src/components/StudyLesson/Template1Std.jsx
@@ -1,6 +1,6 @@
 // Template1Std.jsx
-import React, { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import React, { useState, useEffect, useRef } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import styled from "styled-components";
 import * as C from "../CreateLesson/CreateLessonStyle";
@@ -8,6 +8,10 @@ import * as S from "../SelfStudy/SelfStudyStyle";
 import * as D from "../WordCreateTchr/WordDetailStyle";
 import Back from "/src/assets/icon/back.svg";
 import word from "../../assets/image/word.svg";
+import Pink from "/src/assets/icon/heartpink.svg";
+import White from "/src/assets/icon/heartwhite.svg";
+import reset from "../../assets/icon/reset.svg";
+import hint from "../../assets/icon/hint.svg";
 
 export const Row = styled.div`
   display: flex;
@@ -68,17 +72,39 @@ export const Text = styled.div`
 const Template1Std = () => {
   const template1Id = useLocation().state;
   const [category, setCatergory] = useState(["동물", "식물", "음식"]);
+  const randomCategory = [...category].sort(() => Math.random() - 0.5);
+  // const [dataList, setdataList] = useState([
+  //   {
+  //     img: [word, word, word],
+  //     clicked: [0, 0, 0],
+  //     used: [0, 0, 0],
+  //   },
+  //   {
+  //     category: [...randomCategory],
+  //     clicked: [0, 0, 0],
+  //     used: [0, 0, 0],
+  //   },
+  // ]);
+
+  const navigate = useNavigate();
+  const [feedbackData, setFeedbackData] = useState(null);
+  const [showReward, setShowReward] = useState(false);
+  const answerRef = useRef(false);
+  const [lives, setLives] = useState(2);
+  const [correct, setCorrect] = useState([]);
+  const [hintShow, setHintShow] = useState([]);
+  const [firstTime, setFirstTime] = useState(true);
+  const [word, setWord] = useState([]);
+  const [wordList, setWordList] = useState([]);
+  const [finalAnswer, setfinalAnswer] = useState([]);
   const [line, setLine] = useState([]);
   const [addLine, setAddLine] = useState(["", ""]);
-  const randomCategory = [...category].sort(() => Math.random() - 0.5);
-  const [dataList, setdataList] = useState([
+  const [clicked, setClicked] = useState([
     {
-      img: [word, word, word],
       clicked: [0, 0, 0],
       used: [0, 0, 0],
     },
     {
-      category: [...randomCategory],
       clicked: [0, 0, 0],
       used: [0, 0, 0],
     },
@@ -89,12 +115,11 @@ const Template1Std = () => {
       .get(`https://maeummal.com/api/temp1/get?temp1Id=${template1Id}`)
       .then((response) => {
         if (response.data.isSuccess) {
-          console.log(response.data);
-          // const data = response.data.data.wordCardList;
-          // setWordList(data);
-          // let newWord = [];
-          // data.map((el, index) => (newWord[index] = el.meaning));
-          // setWord(newWord.sort(() => Math.random() - 0.5));
+          const data = response.data.data.wordCardList;
+          setWordList(data);
+          let newWord = [];
+          data.map((el, index) => (newWord[index] = el.meaning));
+          setWord(newWord.sort(() => Math.random() - 0.5));
         }
       })
       .catch((error) => {
@@ -103,34 +128,124 @@ const Template1Std = () => {
   }, []);
 
   const boxClick = (event, index) => {
-    const newDataList = [...dataList];
+    const newClicked = [...clicked];
     const newAddLine = [...addLine];
-    const type = event.target.id === "card" ? 0 : 1;
-    if (!dataList[type].used[index]) {
-      newDataList[type].clicked = [0, 0, 0];
-      newDataList[type].clicked[index] = 1;
-      if (index === 0) newAddLine[type] = "18%";
+    const type = event.target.id === "word" ? 1 : 0;
+    if (!clicked[type].used[index]) {
+      newClicked[type].clicked = [0, 0, 0];
+      newClicked[type].clicked[index] = 1;
+      if (index === 0) newAddLine[type] = "17%";
       else if (index === 1) newAddLine[type] = "50%";
-      else newAddLine[type] = "82%";
+      else newAddLine[type] = "83%";
     }
-    setdataList(newDataList);
+    if (answerRef.current || answerRef.current === 0) {
+      const newAnswer = [...finalAnswer];
+      event.target.id === "word"
+        ? (newAnswer[answerRef.current] = event.target.innerText)
+        : (newAnswer[index] = answerRef.current);
+      setfinalAnswer(newAnswer);
+      answerRef.current = false;
+    } else {
+      answerRef.current =
+        event.target.id === "word" ? event.target.innerText : index;
+    }
+    setClicked(newClicked);
     setAddLine(newAddLine);
   };
   useEffect(() => {
     if (addLine[0] != "" && addLine[1] != "") {
-      setLine([...line, addLine]);
       addLine.map((el, index) => {
-        const newDataList = [...dataList];
+        const newDataList = [...clicked];
         newDataList[index].clicked = [0, 0, 0];
-        if (el === "18%") newDataList[index].used[0] = 1;
+        if (el === "17%") newDataList[index].used[0] = 1;
         else if (el === "50%") newDataList[index].used[1] = 1;
         else newDataList[index].used[2] = 1;
-        setdataList(newDataList);
+        setClicked(newDataList);
       });
+      const newLine = [...line];
+      if (addLine[0] === "17%") newLine[0] = addLine;
+      else if (addLine[0] === "50%") newLine[1] = addLine;
+      else if (addLine[0] === "83%") newLine[2] = addLine;
+      setLine(newLine);
       setAddLine(["", ""]);
     }
   }, [addLine]);
 
+  const handleSubmit = () => {
+    if (line.length === 3 && firstTime) {
+      const newCorrect = [...correct];
+      wordList.map((el, index) => {
+        el.meaning === finalAnswer[index]
+          ? (newCorrect[index] = true)
+          : (newCorrect[index] = false);
+      });
+      setCorrect(newCorrect);
+      setHintShow(newCorrect);
+      setLives(lives - 1);
+      setFirstTime(false);
+    } else if (!firstTime) {
+      const count = correct.filter((element) => true === element).length;
+      if (count != 3) {
+        setLives(lives - 1);
+      }
+      feedback();
+    } else {
+      alert("모두 선을 이어주세요!");
+    }
+  };
+
+  useEffect(() => {
+    const count = correct.filter((element) => true === element).length;
+    if (count === 3) {
+      setLives(2);
+      feedback();
+    }
+  }, [correct]);
+
+  const feedback = () => {
+    const payload = {
+      templateId: template1Id,
+      answerList: finalAnswer,
+      studentId: 25,
+      templateType: "TEMPLATE1",
+    };
+    axios
+      .post("https://maeummal.com/feedback/create", payload, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("key")}`,
+        },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          setShowReward(true);
+          setFeedbackData(response.data);
+        }
+      })
+      .catch((error) => {
+        console.error("Error while create feedback:", error);
+      });
+  };
+
+  const handleReset = () => {
+    setClicked([
+      {
+        clicked: [0, 0, 0],
+        used: [0, 0, 0],
+      },
+      {
+        clicked: [0, 0, 0],
+        used: [0, 0, 0],
+      },
+    ]);
+    setLine([]);
+    setfinalAnswer([]);
+    setCorrect([]);
+  };
+
+  const handleCloseReward = () => {
+    setShowReward(false);
+    navigate("/feedback5", { state: feedbackData });
+  };
   return (
     <>
       <D.ImageWrap>
@@ -138,41 +253,64 @@ const Template1Std = () => {
           <img src={Back} alt="" />
         </a>
       </D.ImageWrap>
+      <D.HeartWrap>
+        {Array.from({ length: 2 }).map((_, index) => (
+          <img key={index} src={index < 2 - lives ? White : Pink} alt="Heart" />
+        ))}
+      </D.HeartWrap>
       <S.AppContainer>
-        <h1>
-          {category.map((el) =>
-            el === category[category.length - 1] ? `${el}` : `${el}, `
-          )}
-          의 차이를 알아봅시다!
-        </h1>
+        <h1>어휘 카드 매칭 게임</h1>
         <Row>
-          <Container style={{ width: "18%", minWidth: "144px" }}>
-            {dataList[0].img.map((el, index) => (
-              <ImgContainer key={index} clickstate={dataList[0].clicked[index]}>
+          {!firstTime && (
+            <Container>
+              {hintShow.map((el, index) => (
+                <HintBox style={{ opacity: el ? "0%" : "100%" }} key={index}>
+                  {wordList[index].description}
+                  <img src={hint} />
+                </HintBox>
+              ))}
+            </Container>
+          )}
+          <Container>
+            {wordList.map((el, index) => (
+              <ImgContainer key={index} clickstate={clicked[0].clicked[index]}>
                 <Circle />
-                <img id="card" src={el} onClick={(e) => boxClick(e, index)} />
+                <img
+                  id={el.meaning}
+                  src={el.image}
+                  onClick={(e) => boxClick(e, index)}
+                />
               </ImgContainer>
             ))}
           </Container>
           <svg height="100%" width="62%">
-            {line.map((el, index) => (
-              <line
-                key={index}
-                x1="0"
-                y1={el[0]}
-                x2="100%"
-                y2={el[1]}
-                stroke="gray"
-                strokeWidth="2"
-              />
-            ))}
+            {line.map(
+              (el, index) =>
+                el && (
+                  <line
+                    key={index}
+                    x1="0%"
+                    y1={el[0]}
+                    x2="100%"
+                    y2={el[1]}
+                    stroke={
+                      correct[index] === undefined
+                        ? "#969696"
+                        : correct[index]
+                        ? "#0000ff"
+                        : "#ff0000"
+                    }
+                    strokeWidth="2"
+                  />
+                )
+            )}
           </svg>
-          <Container style={{ width: "18%" }}>
-            {dataList[1].category.map((el, index) => (
+          <Container>
+            {word.map((el, index) => (
               <Text
                 key={index}
-                id="category"
-                clickstate={dataList[1].clicked[index]}
+                id="word"
+                clickstate={clicked[1].clicked[index]}
                 onClick={(e) => boxClick(e, index)}
               >
                 <Circle style={{ left: "-30px" }} />
@@ -181,10 +319,25 @@ const Template1Std = () => {
             ))}
           </Container>
         </Row>
-        <C.SubmitButton style={{ margin: "0", padding: "0" }}>
-          제출
-        </C.SubmitButton>
+        <Row style={{ justifyContent: "center" }}>
+          <img
+            style={{ height: "30px", marginRight: "10px" }}
+            src={reset}
+            onClick={handleReset}
+          />
+          <C.SubmitButton
+            style={{ margin: "0", padding: "0" }}
+            onClick={handleSubmit}
+          >
+            제출
+          </C.SubmitButton>
+        </Row>
       </S.AppContainer>
+      {showReward && (
+        <ModalOverlay>
+          <Reward onClose={handleCloseReward} />
+        </ModalOverlay>
+      )}
     </>
   );
 };
